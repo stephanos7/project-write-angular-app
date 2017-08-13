f31898f5214eb39082ce9be55338112ac22115f7
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-08-13 21:40:05 +0200

    add signup logic

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
 
-  constructor() { }
+  formInfo = {
+    email: '',
+    password: ''
+  };
+  
+  error: string;
 
-  ngOnInit() {
+  constructor(private router : Router,
+              private session: SessionService) { }
+
+  signup() {
+    this.session.signup(this.formInfo)
+      .subscribe();
+        this.router.navigate(['login']);
   }
 
 }
