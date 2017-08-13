7704c513037e410668e037649a4ffedc7193f05c
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-08-13 21:21:46 +0200

    add session service to handle auth tokens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { appRouting } from './app.routing';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -11,6 +12,9 @@ import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+
+// SERVICES
+import { SessionService } from './services/session.service';
 
 @NgModule({
   declarations: [
@@ -29,7 +33,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     appRouting
     
   ],
-  providers: [],
+  providers: [SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/session.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Rx';
+import { Router, CanActivate } from '@angular/router';
+
+@Injectable()
+export class SessionService implements CanActivate {
+  BASE_URL: string = 'http://localhost:3000';
+
+  public user = {};
+  public token = '';
+  public isAuthenticated = false;
+
+  constructor(
+    private http: Http,
+    private router: Router
+  ) { }
+
+  handleError(e) {
+    return Observable.throw(e.json().message);
+  }
+
+  canActivate(): Observable<boolean> | boolean {
+    let token = localStorage.getItem('token');
+
+    if (token) {
+      let headers = new Headers({ 'Authorization': `JWT ${token}` });
+      let options = new RequestOptions({ headers: headers });
+
+      return this.http.get(`${this.BASE_URL}/private`, options)
+        .map((data) => {
+          if (data) {
+            this.isAuthenticated = true;
+            this.token = token;
+            return true;
+          }
+          return false;
+        })
+        .catch(this.handleError);
+    }
+    else {
+      this.logout();
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+  login(user) {
+    return this.http.post(`${this.BASE_URL}/login`, user)
+      .map(res => {
+        let json = res.json();
+        let token = json.token;
+        let user = json.user;
+
+        if (token) {
+          this.token = token;
+          this.user = {
+            id: user._id,
+            username: user.username
+          }
+          this.isAuthenticated = true;
+        }
+        
+        return this.isAuthenticated;
+
+      }).catch(this.handleError);
+  }
+
+
+    signup(user) {
+    return this.http.post(`${this.BASE_URL}/signup`, user)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  logout() {
+    this.token = '';
+    this.user = {}
+    this.isAuthenticated = false;
+    localStorage.removeItem('token');
+
+    this.router.navigate(['/login']);
+  }
+  
+}
